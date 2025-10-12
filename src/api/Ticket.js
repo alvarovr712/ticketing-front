c8dea76cd105deecd8cfa18b8b4e7f01055075fd
Alvaro <alvarovr712@gmail.com>
2025-10-13 00:21:49 +0200

    vista DetallesTicket ver mensajes y actividad

--- src/api/Ticket.js
+++ src/api/Ticket.js
@@ -1,5 +1,6 @@
 const BASE_URL = "http://localhost:8080";
 
+// -------OBTENER TODOS LOS TICKETS -------
 export const obtenerTodosLosTickets = async(token) => {
     const respuesta = await fetch(`${BASE_URL}/ticket`, {
         method: 'GET',
@@ -15,6 +16,7 @@ export const obtenerTodosLosTickets = async(token) => {
     return await respuesta.json();
 }
 
+// -------OBTENER UN TICKET POR ID -------
 export const obtenerUnTicket = async(token,id) => {
     const respuesta = await fetch(`${BASE_URL}/ticket/${id}`, {
         method: 'GET',
@@ -27,6 +29,21 @@ export const obtenerUnTicket = async(token,id) => {
     if(!respuesta.ok) {
         throw new Error('Error al obtener el ticket');
     }   
+    return await respuesta.json();
+}
+
+//--------OBTENER TODOS LOS GRUPOS QUE HAY -------
+export const obtenerTodosGrupos = async(token) => {
+    const respuesta = await fetch(`${BASE_URL}/grupo/nombres`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    }); 
+    if(!respuesta.ok) {
+        throw new Error('Error al obtener los grupos');
+    }
     return await respuesta.json();
 }
 
--- src/vistas/agente/DetallesTicket.jsx
+++ src/vistas/agente/DetallesTicket.jsx
@@ -1,5 +1,5 @@
-import { use, useEffect, useState } from "react"
-import { obtenerUnTicket } from "../../api/Ticket";
+import { useEffect, useState } from "react"
+import { obtenerTodosGrupos, obtenerUnTicket } from "../../api/Ticket";
 import { useParams } from "react-router-dom";
 import "../../estilos/Chat.css"
 
@@ -9,18 +9,24 @@ const DetallesTicket = () => {
     const [ticket, setTickets] = useState(null);
     const [error, setError] = useState("");
 
-    const [vistaActiva, setVistaActiva] = useState("mensaje")
+    const [vistaActiva, setVistaActiva] = useState("mensajes")
 
     const [urgencia, setUrgencia] = useState("");
     const [impacto, setImpacto] = useState("");
     const [prioridad, setPrioridad] = useState("");
+    const [grupo, setGrupo] = useState("");
+    const [listagrupos, setListagrupos] = useState([])
+
 
 
     useEffect(() => {
+
         const token = localStorage.getItem('token');
 
         obtenerUnTicket(token, id)
             .then(datos => {
+                //Ordenar las anotaciones por fecha
+                datos.anotaciones.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
                 setTickets(datos);
                 setUrgencia((datos.urgencia || "").toLowerCase());
                 setImpacto((datos.impacto || "").toLowerCase());
@@ -29,10 +35,21 @@ const DetallesTicket = () => {
             .catch(error => setError(error.message));
     }, [id]);
 
+    //OBTENER TODOS LOS GRUPOS
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        obtenerTodosGrupos(token)
+            .then(datos => setListagrupos(datos))
+            .catch(error => setError(error.message));
+    }, [])
+
     if (error) return <p style={{ color: "red" }}>{error}</p>
     if (!ticket) return <p> Cargando ticket...</p>
 
     console.log("Urgencia actual:", urgencia);
+
+
 
     return (
 
@@ -41,12 +58,12 @@ const DetallesTicket = () => {
 
                 <div className="col-md-8 d-flex flex-column" style={{ height: "90vh" }}>
                     {/*Descripcion del ticket*/}
-                    <div className="mb-2 border rounded p-3 bg-white" style={{ height: "15vh" }}>
-                        <h5>Descripción</h5>
+                    <div className="ticket-descripcion mb-2" style={{ height: "15vh" }}>
+                        <h5><strong>Descripción:</strong></h5>
                         <p>{ticket.descripcion}</p>
                     </div>
                     {/*tabs para cambiar entre mensaje y actividad*/}
-                    <ul className="nav nav-tabs ">
+                    <ul className="nav nav-tabs ticket-tabs ">
                         <li className="nav-item">
                             <span
                                 className={`nav-link ${vistaActiva === "mensajes" ? "active" : ""}`}
@@ -68,25 +85,59 @@ const DetallesTicket = () => {
                     </ul>
                     {/*Mensaje y actividad (tabs)*/}
                     {vistaActiva === "mensajes" ? (
-                        <div className="flex-grow-1 overflow-auto border rounded p-5 mb-2" style={{ backgroundColor: "#f8f9fa" }}>
-                            <div className="mensaje agente">
-                                <div className="cabecera"><strong>Agente</strong> · 11 oct 2025 · 16:45</div>
-                                <div className="contenido">Hola, ¿en qué puedo ayudarte?</div>
+                        <div className="flex-grow-1 overflow-auto " style={{ backgroundColor: "#f8f9fa" }}>
+                            <div className="flex-grow-1 overflow-auto  mb-2" style={{ backgroundColor: "#f8f9fa" }}>
+                                {ticket.anotaciones.map((anotacion, index) => {
+                                    const perfil = anotacion.usuario.perfil.nombre
+                                    const nombre = `${anotacion.usuario.nombre} ${anotacion.usuario.apellidos}`;
+                                    const clase = anotacion.visibilidadTicket === 0 ? "privado" : "publico";
+                                    const fecha = new Date(anotacion.fecha).toLocaleString("es-ES", {
+                                        day: "2-digit", month: "short", year: "numeric", hour: "2-digit", minute: "2-digit"
+                                    });
+
+                                    return (
+                                        <div key={index} className={`mensaje ${clase}`}>
+                                            <div className="cabecera">
+                                                <strong>{nombre}</strong> - {perfil} , {fecha}
+                                            </div>
+                                            <div className="contenido">
+                                                {anotacion.descripcion}
+                                            </div>
+                                            {/* Botones para editar y borrar cada mensaje */}
+                                            <div className="acciones-mensaje">
+                                                <button className="btn-accion">Editar</button>
+                                                <button className="btn-accion">Borrar</button>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
                             </div>
-                            <div className="mensaje solicitante">
-                                <div className="cabecera"><strong>Cliente</strong> · 11 oct 2025 · 16:46</div>
-                                <div className="contenido">Tengo un problema con el acceso.</div>
-                            </div>
+
                         </div>
                     ) : (
                         <div className="flex-grow-1 overflow-auto border rounded p-4 mb-2 bg-white">
-                            <h5>Actividad del ticket</h5>
+                            <h5 className="text-center mb-4" style={{ fontWeight: "bold", textDecoration: "underline" }}>Actividad del ticket</h5>
                             <ul className="list-unstyled">
-                                <li>🟢 Ticket creado el {new Date(ticket.fechaCreacion).toLocaleDateString()}</li>
-                                <li>✉️ Mensaje enviado por cliente</li>
-                                <li>✉️ Respuesta del agente</li>
-                                <li>🗑️ Mensaje eliminado por agente</li>
-                                {/* Puedes añadir más eventos aquí */}
+                                <li className="mb-3">🟢 <strong>Ticket creado el {new Date(ticket.fechaCreacion).toLocaleDateString("es-ES")}</strong></li>
+
+                                {ticket.historiales
+                                    .map((historial, index) => {
+                                        const nombre = `${historial.usuario.nombre} ${historial.usuario.apellidos}`;
+                                        const perfil = historial.usuario.perfil.nombre;
+                                        const fecha = new Date(historial.fecha).toLocaleString("es-ES", {
+                                            day: "2-digit",
+                                            month: "short",
+                                            year: "numeric",
+                                            hour: "2-digit",
+                                            minute: "2-digit"
+                                        });
+
+                                        return (
+                                            <li key={index} className="mb-3">
+                                                🎫 <strong>{nombre} ({perfil}) · {historial.detalles} · {fecha}</strong>
+                                            </li>
+                                        );
+                                    })}
                             </ul>
                         </div>
                     )}
@@ -111,18 +162,18 @@ const DetallesTicket = () => {
                 </div>
 
                 {/*Columna derecha */}
-                <div className="col-md-4">
+                <div className="col-md-4 ticket-detalles">
                     <h2 style={{ textAlign: "center" }}>Detalles del ticket</h2>
                     <br />
                     <div className="d-flex justify-content-between align-items-center">
                         <h5>
-                            Asunto: <span className="fw-normal ms-2">{ticket.asunto}</span>
+                            <strong>Asunto:</strong> <span className="fw-normal ms-2">{ticket.asunto}</span>
                         </h5>
                     </div>
-                    <br/>
+                    <br />
                     <div className="d-flex justify-content-between align-items-center">
                         <h5>
-                            Fecha de creación:<span className="fw-normal ms-2">{ticket.fechaCreacion}</span>
+                            <strong>Fecha de creación:</strong><span className="fw-normal ms-2 ">{ticket.fechaCreacion}</span>
                         </h5>
 
                     </div>
@@ -130,7 +181,7 @@ const DetallesTicket = () => {
                     <div className="mt-4">
                         {/* Urgencia */}
                         <div className="mb-3">
-                            <label htmlFor="urgencia" className="form-label">Urgencia</label>
+                            <label htmlFor="urgencia" className="form-label"><strong>Urgencia</strong></label>
                             <select id="urgencia" className="form-select" value={urgencia} onChange={(e) => setUrgencia(e.target.value)}>
                                 <option value="">Selecciona urgencia</option>
                                 <option value="baja">BAJA</option>
@@ -141,7 +192,7 @@ const DetallesTicket = () => {
 
                         {/* Impacto */}
                         <div className="mb-3">
-                            <label htmlFor="impacto" className="form-label">Impacto</label>
+                            <label htmlFor="impacto" className="form-label"><strong>Impacto</strong></label>
                             <select id="impacto" className="form-select" value={impacto} onChange={(e) => setImpacto(e.target.value)}>
                                 <option value="">Selecciona impacto</option>
                                 <option value="bajo">BAJO</option>
@@ -152,7 +203,7 @@ const DetallesTicket = () => {
 
                         {/* Prioridad */}
                         <div className="mb-3">
-                            <label htmlFor="prioridad" className="form-label">Prioridad</label>
+                            <label htmlFor="prioridad" className="form-label"><strong>Prioridad</strong></label>
                             <select id="prioridad" className="form-select" value={prioridad} onChange={(e) => setPrioridad(e.target.value)}>
                                 <option value="">Selecciona prioridad</option>
                                 <option value="baja">BAJA</option>
@@ -160,6 +211,27 @@ const DetallesTicket = () => {
                                 <option value="alta">ALTA</option>
                             </select>
                         </div>
+                        {/*Mostrar grupo*/}
+                        <div className="mb-3">
+                            <label htmlFor="grupo" className="form-label"><strong>Asignar Tecnico</strong></label>
+                            <select id="grupo" className="form-select" value={grupo} onChange={(e) => setGrupo(e.target.value)}
+                            >
+
+                                <option value="">Asigna un tecnico</option>
+                                {listagrupos.map((nombre, index) => (
+                                    <option key={index} value={nombre}>{nombre}</option>
+                                ))}
+
+                            </select>
+                        </div>
+
+                        {/*Boton para guardar los cambios*/}
+                        <div className="d-grid mt-5">
+                            <button className="btn btn-success">
+                                Guardar cambios
+                            </button>
+                        </div>
+
                     </div>
 
 
