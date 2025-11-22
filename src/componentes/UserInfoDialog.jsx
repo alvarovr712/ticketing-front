a8352c6a6d0c2ed1b5be8052ba6896f75573ef55
Carlos <carlos@local.es>
2025-11-22 21:32:10 +0100

    Cambios en usuarios para actualizar al cambiar, editar o desactivar usuarios

--- src/componentes/UserInfoDialog.jsx
+++ src/componentes/UserInfoDialog.jsx
@@ -10,39 +10,22 @@ const userInfoDialog = ({ user, open, close }) => {
 	const [editMode, setEditMode] = useState(false);
 	const [readOnly, setReadOnly] = useState(true);
 
-  const handleClose = (event, reason) => {
-    if (reason && reason === 'backdropClick') return;
-    setUserData({ ...user });
-    close();
-  };
+	const handleClose = (event, reason) => {
+		if (reason && reason === 'backdropClick') return;
 
-  const handleSaveUser = async () => {
-    const perfiles = {
-      "ADMIN": 1,
-      "TECNICO": 2,
-      "AGENTE": 3,
-      "SOLICITANTE": 4,
-    };
+		setUserData({ ...user });
 
-    userData.perfil = perfiles[userData.perfil];
-    console.log("Guardando usuario...", userData);
+		close();
+	}
 
-    actualizarUsuario(localStorage.getItem('token'), userData.id, userData)
-      .then((ok) => {
-        if (ok) {
-          console.log("Usuario actualizado correctamente.");
-          setEditMode(false);
-          handleClose();
-        } else {
-          console.log("Error al actualizar el usuario.");
-        }
-      })
-      .catch((err) => {
-        console.log("Error al actualizar el usuario:", err);
-      });
-  };
+	const handleSaveUser = async () => {
+		const perfiles = {
+			"ADMIN": 1,
+			"TECNICO": 2,
+			"AGENTE": 3,
+			"SOLICITANTE": 4,
+		}
 
-<<<<<<< HEAD
 		const payload = { ...userData, perfil: perfiles[userData.perfil] }
 
 		setUserData(payload);
@@ -54,64 +37,30 @@ const userInfoDialog = ({ user, open, close }) => {
 				if (ok) {
 					console.log("Usuario actualizado correctamente.");
 					setEditMode(false);
-=======
-  const handleNoSave = () => {
-    setUserData({ ...user });
-    setEditMode(false);
-  };
 
-  const handleChosenGrupo = (event) => {
-    setUserData({ ...userData, idGrupo: event.target.value });
-  };
+					handleClose();
+				} else {
+					console.log("Error al actualizar el usuario.");
+				}
 
-  useEffect(() => {
-    if (user.perfil === 'SOLICITANTE') {
-      setEsSolicitante(true);
-      setBoxSize(4);
-    } else {
-      setEsSolicitante(false);
-      setBoxSize(6);
-    }
-  }, [user]);
->>>>>>> fa35cc896ca90fb5b587aaa14ba6f871e0f3fd8b
 
-  useEffect(() => {
-    if (!editMode) {
-      setUserData({ ...user });
-    }
-  }, [editMode, user]);
+			}).catch((err) => {
+				console.log("Error al actualizar el usuario:", err);
+			});
 
-  return (
-    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth={true}>
-      <DialogTitle>Información del Usuario</DialogTitle>
+	}
 
-      <Grid container rowSpacing={2} columnSpacing={2} padding={2} alignItems="center" justifyContent="center">
-        <Grid item size={2}>
-          {!editMode && (
-            <Button variant="contained" onClick={() => setEditMode(true)}>Editar</Button>
-          )}
-          {editMode && (
-            <Button variant="contained" onClick={handleSaveUser}>Guardar</Button>
-          )}
-        </Grid>
+	const handleNoSave = () => {
+		setUserData({ ...user });
+		setEditMode(false);
+	}
 
-        <Grid item size={2}>
-          {editMode && (
-            <Button variant="outlined" onClick={handleNoSave}>Cancelar</Button>
-          )}
-        </Grid>
-
-        <Grid item size={6}></Grid>
-
-<<<<<<< HEAD
 	const handleChosenGrupo = (event) => {
 		console.log(userData)
 		setUserData({ ...userData, idGrupo: event.target.value });
 	}
 	useEffect(() => {
 		if (!user) return;
-
-		setUserData({ ...user });
 
 		if (user.perfil === 'SOLICITANTE') {
 			setEsSolicitante(true);
@@ -126,92 +75,182 @@ const userInfoDialog = ({ user, open, close }) => {
 
 	useEffect(() => {
 		setReadOnly(!editMode);
-=======
-        <Grid item size={2}>
-          <Button variant="contained" onClick={handleClose}>Cerrar</Button>
-        </Grid>
 
-        <Grid item size={boxSize}>
-          <TextField
-            label="Nombre"
-            variant="standard"
-            value={userData.nombre || ""}
-            onChange={({ target }) => setUserData({ ...userData, nombre: target.value })}
-            InputProps={{ readOnly: !editMode }}
-          />
-        </Grid>
+		if (!editMode) {
+			setUserData({ ...user });
+		}
+	}, [editMode]);
 
-        <Grid item size={boxSize}>
-          <TextField
-            label="Apellidos"
-            variant="standard"
-            value={userData.apellidos || ""}
-            onChange={({ target }) => setUserData({ ...userData, apellidos: target.value })}
-            InputProps={{ readOnly: !editMode }}
-          />
-        </Grid>
->>>>>>> fa35cc896ca90fb5b587aaa14ba6f871e0f3fd8b
+	return (
+		<Dialog
+			open={open}
+			onClose={handleClose}
+			maxWidth="md"
+			fullWidth={true}
+		>
+			<DialogTitle>
+				Información del Usuario
+			</DialogTitle>
 
-        <Grid item size={boxSize}>
-          <TextField
-            label="Email"
-            variant="standard"
-            value={userData.email || ""}
-            onChange={({ target }) => setUserData({ ...userData, email: target.value })}
-            InputProps={{ readOnly: !editMode }}
-          />
-        </Grid>
+			<Grid
+				container
+				rowSpacing={2}
+				columnSpacing={2}
+				padding={2}
+				alignItems="center"
+				alignContent={"center"}
+				justifyContent={"center"}
+			>
 
-        {!esSolicitante && (
-          <Grid item size={boxSize}>
-            <Select
-              value={userData.idGrupo || 0}
-              onChange={handleChosenGrupo}
-              disabled={!editMode}
-            >
-              {grupos.map(({ id, nombre }) => (
-                <MenuItem key={id} value={id}>{nombre}</MenuItem>
-              ))}
-            </Select>
-          </Grid>
-        )}
+				<Grid
+					item
+					size={2}
 
-        {esSolicitante && (
-          <>
-            <Grid item size={boxSize}>
-              <TextField
-                label="Empresa"
-                variant="standard"
-                value={userData.empresa || ""}
-                onChange={({ target }) => setUserData({ ...userData, empresa: target.value })}
-                InputProps={{ readOnly: !editMode }}
-              />
-            </Grid>
+				>
+					{!editMode && (
+						<Button variant="contained" onClick={() => setEditMode(true)}>Editar</Button>
+					)}
 
-            <Grid item size={boxSize}>
-              <TextField
-                label="Teléfono"
-                variant="standard"
-                value={userData.telefono || ""}
-                onChange={({ target }) => setUserData({ ...userData, telefono: target.value })}
-                InputProps={{ readOnly: !editMode }}
-              />
-            </Grid>
+					{editMode && (
+						<Button variant="contained" onClick={() => handleSaveUser()}>Guardar</Button>
+					)}
 
-            <Grid item size={boxSize}>
-              <TextField
-                label="CIF"
-                variant="standard"
-                value={userData.cif || ""}
-                onChange={({ target }) => setUserData({ ...userData, cif: target.value })}
-                InputProps={{ readOnly: !editMode }}
-              />
-            </Grid>
-          </>
-        )}
-      </Grid>
-    </Dialog>
-  );
-};
+				</Grid>
 
-export default UserInfoDialog;
+				<Grid
+					item
+					size={2}
+				>
+					{editMode && (
+						<Button variant="outlined" onClick={() => handleNoSave()}>Cancelar</Button>
+					)}
+				</Grid>
+
+				<Grid
+					item
+					size={6}
+				>
+
+				</Grid>
+
+				<Grid
+					item
+					size={2}
+				>
+					<Button variant="contained" onClick={() => handleClose()}>Cerrar</Button>
+				</Grid>
+
+				<Grid item size={boxSize}>
+					<TextField
+						label="Nombre"
+						variant="standard"
+						defaultValue={user.nombre}
+						onChange={({ target }) => setUserData({ ...userData, nombre: target.value })}
+						slotProps={{
+							input: {
+								readOnly: !editMode,
+							}
+						}}
+					/>
+				</Grid>
+
+				<Grid item size={boxSize}>
+					<TextField
+						label="Apellidos"
+						variant="standard"
+						defaultValue={user.apellidos}
+						onChange={({ target }) => setUserData({ ...userData, apellidos: target.value })}
+						slotProps={{
+							input: {
+								readOnly: !editMode,
+							}
+						}}
+					/>
+				</Grid>
+
+				<Grid item size={boxSize}>
+					<TextField
+						label="Email"
+						variant="standard"
+						defaultValue={user.email}
+						onChange={({ target }) => setUserData({ ...userData, email: target.value })}
+						slotProps={{
+							input: {
+								readOnly: !editMode,
+							}
+						}}
+					/>
+				</Grid>
+
+				{!esSolicitante && (
+					<Grid item size={boxSize}>
+						<Select
+							defaultValue={user.idGrupo}
+							onChange={handleChosenGrupo}
+							slotProps={{
+								input: {
+									readOnly: !editMode,
+								}
+							}}
+						>
+							{grupos.map(({ id, nombre }) => (
+								<MenuItem key={id} value={id}>{nombre}</MenuItem>
+							))}
+						</Select>
+					</Grid>
+				)}
+
+				{esSolicitante && (
+					<Grid item size={boxSize}>
+						<TextField
+							label="Empresa"
+							variant="standard"
+							defaultValue={user.empresa}
+							onChange={({ target }) => setUserData({ ...userData, empresa: target.value })}
+							slotProps={{
+								input: {
+									readOnly: !editMode,
+								}
+							}}
+						/>
+					</Grid>
+				)}
+
+				{esSolicitante && (
+					<Grid item size={boxSize}>
+						<TextField
+							label="Teléfono"
+							variant="standard"
+							defaultValue={user.telefono}
+							onChange={({ target }) => setUserData({ ...userData, telefono: target.value })}
+							slotProps={{
+								input: {
+									readOnly: !editMode,
+								}
+							}}
+						/>
+					</Grid>
+				)}
+
+				{esSolicitante && (
+					<Grid item size={boxSize}>
+						<TextField
+							label="CIF"
+							variant="standard"
+							defaultValue={user.cif}
+							onChange={({ target }) => setUserData({ ...userData, cif: target.value })}
+							slotProps={{
+								input: {
+									readOnly: !editMode,
+								}
+							}}
+						/>
+					</Grid>
+				)}
+
+			</Grid>
+		</Dialog>
+	)
+}
+
+export default userInfoDialog;
